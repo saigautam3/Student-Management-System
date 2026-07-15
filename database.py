a18e6ca1c9ffc8533f3067a80bfc7c2cfc7a28f1
Sai Gautam <saigautam315@gmail.com>
2026-07-16 00:39:53 +0530

    Modified database.py

--- database.py
+++ database.py
@@ -1,27 +1,24 @@
-# import psycopg2
+import psycopg2
+def get_connection():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="studentdb",
+        user="postgres",
+        password="root",
+        port="5432"
+    )
+    return conn
 
+
+#import os
+#import psycopg2
 #def get_connection():
+
 #    conn = psycopg2.connect(
-#       host="localhost",
-#        database="studentdb",
-#        user="postgres",
-#        password="root",
-#        port="5432"
+#       host=os.environ.get("dpg-d9bt8crbc2fs73b1dj50-a"),
+#        database=os.environ.get("studentdb_cle3"),
+#        user=os.environ.get("studentdb_cle3_user"),
+#        password=os.environ.get("root"),
+#        port=os.environ.get("5432")
 #    )
 #    return conn
-
-import os
-import psycopg2
-
-
-def get_connection():
-
-    conn = psycopg2.connect(
-        host=os.environ.get("dpg-d9bt8crbc2fs73b1dj50-a"),
-        database=os.environ.get("studentdb_cle3"),
-        user=os.environ.get("studentdb_cle3_user"),
-        password=os.environ.get("root"),
-        port=os.environ.get("5432")
-    )
-
-    return conn

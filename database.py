f511ee063d9104916e4636228a116613556425d9
Sai Gautam <saigautam315@gmail.com>
2026-07-16 00:31:08 +0530

    Modified database.py

--- database.py
+++ database.py
@@ -20,7 +20,7 @@ def get_connection():
         host=os.environ.get("dpg-d9bt8crbc2fs73b1dj50-a"),
         database=os.environ.get("studentdb_cle3"),
         user=os.environ.get("studentdb_cle3_user"),
-        password=os.environ.get("HKC4gBpNhqpJRjPmWTPlUFIPIk4xMMZN"),
+        password=os.environ.get("root"),
         port=os.environ.get("5432")
     )
 

dd9e04bcdc8751cb8f75e88a07e552a1d8373fb0
Sai Gautam <saigautam315@gmail.com>
2026-07-16 00:16:54 +0530

    Modified database.py

--- database.py
+++ database.py
@@ -1,3 +1,15 @@
+# import psycopg2
+
+#def get_connection():
+#    conn = psycopg2.connect(
+#       host="localhost",
+#        database="studentdb",
+#        user="postgres",
+#        password="root",
+#        port="5432"
+#    )
+#    return conn
+
 import os
 import psycopg2
 
